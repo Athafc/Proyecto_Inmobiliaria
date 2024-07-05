7303485342ab621fed31b8d6bb19e926eb341662
Athala <athalafarfancossio@gmail.com>
2024-07-05 02:16:13 -0400

    app validators

---
+++ arriendos/app/migrations/services.py
@@ -0,0 +1,11 @@
+from .models import Inmueble, Tipo_inmueble, User
+
+def get_all_inmuebles():
+    pass
+
+def insertar_inmueble():
+    pass
+def actualizar_inmueble():
+    pass
+def borrar_inmueble():
+    pass
--- arriendos/app/models.py
+++ arriendos/app/models.py
@@ -1,11 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator
+from django.core.exceptions import ValidationError
 
 # Create your models here.
 
+def validate_min_value(value):
+    if value<0:
+        raise ValidationError(
+            'El valor deber ser positivo'
+        )
+
 class Usuario(models.Model):
     id_usuario = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.id_usuario.username
 
 class Inmueble(models.Model):
     id_usuario = models.ForeignKey('auth.User', on_delete=models.CASCADE)
@@ -14,17 +24,27 @@ class Inmueble(models.Model):
     id_region = models.ForeignKey('Region' , on_delete=models.CASCADE)
     nombre_inmueble =models.CharField(max_length=200)
     m2_construido = models.FloatField()
-    numero_banos = models.IntegerField(validators= [MinValueValidator(0)], default=0)
-    numero_habitaciones = models.IntegerField(validators= [MinValueValidator(0)], default=0)
+    numero_banos = models.PositiveBigIntegerField(default=0)
+    numero_habitaciones = models.IntegerField(validators= [validate_min_value], default=0)
     direccion = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.nombre_inmueble
+
 
 
 class Region(models.Model):
     region = models.TextField()
+
+    def __str__(self):
+        return self.region
     
 
 class Comuna(models.Model):
     comuna = models.TextField()
+
+    def __str__(self):
+        return self.comuna
     
 
 class Tipo_inmueble(models.Model):
@@ -34,6 +54,9 @@ class Tipo_inmueble(models.Model):
         ('Parcela', 'Parcela'),
     ]
     tipo = models.TextField(choices=CHOICES)
+
+    def __str__(self):
+        return self.tipo
     
 
 class Tipo_usuario(models.Model):
@@ -42,13 +65,20 @@ class Tipo_usuario(models.Model):
         ('Arrendatario', 'Arrendatario')
     ]
     tipo = models.CharField(choices=CHOICES)
+
+    def __str__(self):
+        return self.tipo
   
 
 class Perfil(models.Model):
     usuario = models.OneToOneField('auth.User', on_delete=models.CASCADE)
+    tipo_usuario = models.ForeignKey('Tipo_usuario' , on_delete=models.CASCADE)
     rut = models.TextField()
     direccion = models.TextField()
     telefono = models.TextField()
     correo = models.EmailField()
+
+    def __str__(self):
+        return self.usuario.username
 
     
